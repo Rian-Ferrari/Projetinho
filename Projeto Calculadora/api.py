300e528670b8c9faf34aa5565ecd72a1f872d9aa
Rian Ferrari <rianferrari37@gmail.com>
2023-08-13 14:45:02 -0300

    subindo html,js e py pré pronto

---
+++ Projeto Calculadora/api.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+SocketIO = SocketIO(app, cors_allowed_origins="*")
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+SocketIO.run(app, host="localhost")
